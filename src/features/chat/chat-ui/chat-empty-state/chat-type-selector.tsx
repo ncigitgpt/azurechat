66d2942db56ede4be350a5d6535a038223c752a6
ncigitgpt <nci.gpt.tutorial@gmail.com>
2024-01-11 08:51:03 +0900

    日本語対応

--- src/features/chat/chat-ui/chat-empty-state/chat-type-selector.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-type-selector.tsx
@@ -1,14 +1,16 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { FileText, MessageCircle } from "lucide-react";
+import { FileText, MessageCircle,File,Globe } from "lucide-react";
 import { FC } from "react";
 import { ChatType } from "../../chat-services/models";
 import { useChatContext } from "../chat-context";
+import { useSession } from "next-auth/react";
 
 interface Prop {
   disable: boolean;
 }
 
 export const ChatTypeSelector: FC<Prop> = (props) => {
+  const { data: session } = useSession();
   const { chatBody, onChatTypeChange } = useChatContext();
 
   return (
@@ -17,20 +19,20 @@ export const ChatTypeSelector: FC<Prop> = (props) => {
       onValueChange={(value) => onChatTypeChange(value as ChatType)}
     >
       <TabsList className="grid w-full grid-cols-2 h-12 items-stretch">
-        <TabsTrigger
+       <TabsTrigger
           value="simple"
-          className="flex gap-2"
+          className="flex gap-1"
           disabled={props.disable}
         >
-          <MessageCircle size={20} /> General
-        </TabsTrigger>
+          <MessageCircle size={20} /> 通常利用
+        </TabsTrigger>    
         <TabsTrigger
-          value="data"
-          className="flex gap-2"
+          value="web"
+          className="flex gap-1"
           disabled={props.disable}
         >
-          <FileText size={20} /> File
-        </TabsTrigger>
+          <Globe size={20} /> Web検索
+        </TabsTrigger>   
       </TabsList>
     </Tabs>
   );
